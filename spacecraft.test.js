aaa458b4ee3cb6ce6a793f642b259849ed483a00
halpaniraj.hr4823@gmail.com <halpaniraj.hr4823@gmail.com>
2023-08-29 22:04:44 +0530

    🧪 Tests for 'moveBackward' function

--- spacecraft.test.js
+++ spacecraft.test.js
@@ -22,7 +22,28 @@ describe('Spacecraft Control Tests', () => {
             });
         });
     });
-    
+
+    describe('Move Backward', () => {
+        const testCases = [
+            { initialDirection: 'N', expectedPosition: { x: 0, y: -1, z: 0 } },
+            { initialDirection: 'S', expectedPosition: { x: 0, y: 1, z: 0 } },
+            { initialDirection: 'E', expectedPosition: { x: -1, y: 0, z: 0 } },
+            { initialDirection: 'W', expectedPosition: { x: 1, y: 0, z: 0 } },
+            { initialDirection: 'Up', expectedPosition: { x: 0, y: 0, z: -1 } },
+            { initialDirection: 'Down', expectedPosition: { x: 0, y: 0, z: 1 } },
+        ];
+
+        testCases.forEach(({ initialDirection, expectedPosition }) => {
+            test(`From facing ${initialDirection}`, () => {
+                const spacecraft = new Spacecraft({ x: 0, y: 0, z: 0 }, initialDirection);
+                spacecraft.moveBackward();
+
+                expect(spacecraft.position).toEqual(expectedPosition);
+                expect(spacecraft.direction).toBe(initialDirection);
+            });
+        });
+    });
+
 });
 
 
