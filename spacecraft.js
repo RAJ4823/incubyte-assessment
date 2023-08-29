708ac50ec70c52955d39389e873c31fbd8317f02
halpaniraj.hr4823@gmail.com <halpaniraj.hr4823@gmail.com>
2023-08-29 21:57:59 +0530

    ✅ Implemented 'moveForward' function

--- spacecraft.js
+++ spacecraft.js
@@ -3,6 +3,29 @@ class Spacecraft {
         this.position = position;
         this.direction = direction;
     }
+
+    moveForward() {
+        switch (this.direction) {
+            case 'N':
+                this.position.y += 1;
+                break;
+            case 'S':
+                this.position.y -= 1;
+                break;
+            case 'E':
+                this.position.x += 1;
+                break;
+            case 'W':
+                this.position.x -= 1;
+                break;
+            case 'Up':
+                this.position.z += 1;
+                break;
+            case 'Down':
+                this.position.z -= 1;
+                break;
+        }
+    }
 };
 
 module.exports = Spacecraft;
