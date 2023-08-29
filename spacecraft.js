588e3f8a6d5fa1b93859e693e3614328380a1bc9
halpaniraj.hr4823@gmail.com <halpaniraj.hr4823@gmail.com>
2023-08-29 22:05:19 +0530

    ✅ Implemented 'moveBackward' function

--- spacecraft.js
+++ spacecraft.js
@@ -26,6 +26,29 @@ class Spacecraft {
                 break;
         }
     }
+
+    moveBackward() {
+        switch (this.direction) {
+            case 'N':
+                this.position.y -= 1;
+                break;
+            case 'S':
+                this.position.y += 1;
+                break;
+            case 'E':
+                this.position.x -= 1;
+                break;
+            case 'W':
+                this.position.x += 1;
+                break;
+            case 'Up':
+                this.position.z -= 1;
+                break;
+            case 'Down':
+                this.position.z += 1;
+                break;
+        }
+    }
 };
 
 module.exports = Spacecraft;
