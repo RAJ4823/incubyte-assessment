be5ac1c849ecf3269ff405d64557a5baaae4d87b
halpaniraj.hr4823@gmail.com <halpaniraj.hr4823@gmail.com>
2023-08-29 21:56:50 +0530

    🧪 Tests for 'moveForward' function

--- spacecraft.test.js
+++ spacecraft.test.js
@@ -2,6 +2,27 @@ const Spacecraft = require('./spacecraft');
 
 describe('Spacecraft Control Tests', () => {
 
+    describe('Move Forward', () => {
+        const testCases = [
+            { initialDirection: 'N', expectedPosition: { x: 0, y: 1, z: 0 } },
+            { initialDirection: 'S', expectedPosition: { x: 0, y: -1, z: 0 } },
+            { initialDirection: 'E', expectedPosition: { x: 1, y: 0, z: 0 } },
+            { initialDirection: 'W', expectedPosition: { x: -1, y: 0, z: 0 } },
+            { initialDirection: 'Up', expectedPosition: { x: 0, y: 0, z: 1 } },
+            { initialDirection: 'Down', expectedPosition: { x: 0, y: 0, z: -1 } },
+        ];
+
+        testCases.forEach(({ initialDirection, expectedPosition }) => {
+            test(`From facing ${initialDirection}`, () => {
+                const spacecraft = new Spacecraft({ x: 0, y: 0, z: 0 }, initialDirection);
+                spacecraft.moveForward();
+
+                expect(spacecraft.position).toEqual(expectedPosition);
+                expect(spacecraft.direction).toBe(initialDirection);
+            });
+        });
+    });
+    
 });
 
 
